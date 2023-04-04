f9b0ec141ed43e70a2350382fc5950dfe426fea0
YuliaKhyzhuk <anastezy1@gmail.com>
2023-04-04 04:06:42 +0300

    ContactsList refactored

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { DeleteContactButton, ContactsContainer, ContactsData } from './ContactsList.styled';
+// import PropTypes from 'prop-types';
+import {
+  DeleteContactButton,
+  ContactsContainer,
+  ContactsData,
+} from './ContactsList.styled';
 
+import { getContacts, getFilter } from 'redux/selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contactsSlice';
 
-export const ContactsList = ({ contacts, onDeleteContact }) => {
+export const ContactsList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const filterQuery = useSelector(getFilter);
+  const normalizedFilter = filterQuery.toLowerCase();
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+
   return (
     <ContactsContainer>
-      {contacts.map(({ name, number, id }) => (
-        <ContactsData key={id} className="ContactsList__item">
+      {filteredContacts.map(({ name, number, id }) => (
+        <ContactsData key={id}>
           {name}: {number}
           <DeleteContactButton
             type="button"
-            className="ContactDelete__btn"
-            onClick={() => onDeleteContact(id)}
+            onClick={() => dispatch(deleteContact(id))}
           >
             Delete
           </DeleteContactButton>
@@ -22,10 +37,10 @@ export const ContactsList = ({ contacts, onDeleteContact }) => {
   );
 };
 
-ContactsList.propTypes = {
-  contacts: PropTypes.arrayOf(PropTypes.shape({ contact: PropTypes.object }))
-    .isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
-};
+// ContactsList.propTypes = {
+//   contacts: PropTypes.arrayOf(PropTypes.shape({ contact: PropTypes.object }))
+//     .isRequired,
+//   onDeleteContact: PropTypes.func.isRequired,
+// };
 
 export default ContactsList;
